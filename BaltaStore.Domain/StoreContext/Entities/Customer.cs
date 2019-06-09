aec3a30f77b0fce5ba5f6d0867ab004be8d035ae
Francelio Alencar <francelio.si@gmail.com>
2019-06-09 11:37:20 -0300

    Trabalhando com Collections

--- BaltaStore.Domain/StoreContext/Entities/Customer.cs
+++ BaltaStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,12 +1,15 @@
 using BaltaStore.Domain.StoreContext.ValueObjects;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BaltaStore.Domain.StoreContext.Entities
 {
     public class Customer
 	{
-        public Customer(
+		private readonly IList<Address> _addresses;
+
+		public Customer(
             Name name,
             Document document,
             Email email,
@@ -16,14 +19,22 @@ namespace BaltaStore.Domain.StoreContext.Entities
             Document = document;
             Email = email;
             Phone = phone;
-			Addresses = new List<Address>();
+			_addresses = new List<Address>();
         }
 		public Name Name { get; private set; }
 		public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
-        //sobrecrevendo o metodo Tostring caso contrario return namespace+custumer
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
+
+		public void AddAddress(Address address)
+		{
+			//Validar o endereço
+			//Adicionar o endereço
+			_addresses.Add(address);
+		}
+
+		//sobrecrevendo o metodo Tostring caso contrario return namespace+custumer
         public override string ToString()
         {
             return Name.ToString();
--- BaltaStore.Domain/StoreContext/Entities/Order.cs
+++ BaltaStore.Domain/StoreContext/Entities/Order.cs
@@ -1,32 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using BaltaStore.Domain.StoreContext.Enums;
 
 namespace BaltaStore.Domain.StoreContext.Entities
 {
     public class Order 
     {
+		private readonly IList<OrderItem> _items;
+		private readonly IList<Delivery> _deliveries;
 		public Order(Customer custumer)
 		{
 			Customer = custumer;
 			Number = Guid.NewGuid().ToString().Replace("-","").Substring(0,8).ToUpper();
 			CreateDate = DateTime.Now;
 			Status = EOrderStatus.Created;
-			Items = new List<OrderItem>();
-			Deliveries = new List<Delivery>();
+			_items = new List<OrderItem>();
+			_deliveries = new List<Delivery>();
 		}
 		public Customer Customer  { get; private set; }
         public string Number { get;private  set; }
         public DateTime CreateDate { get; private set; }
         public EOrderStatus Status { get; private set; }
-		public IReadOnlyCollection<OrderItem> Items { get; private set; }
-		public IReadOnlyCollection<Delivery> Deliveries { get; private set; }
+		public IReadOnlyCollection<OrderItem> Items => _items.ToArray();
+		public IReadOnlyCollection<Delivery> Deliveries => _deliveries.ToArray();
 		//IReadOnlyCollection coleção somente leitura
 
 		public void AddItem(OrderItem item)
 		{
 			//validar item se tem em estoque, se o preco esta correto
 			//adiciona item  ao pedido
+			_items.Add(item);
+		}
+		public void AddDelivery(Delivery delivery)
+		{
+			//validar item se tem em estoque, se o preco esta correto
+			//adiciona item  ao pedido
+			_deliveries.Add(delivery);
 		}
 		//To Place An Order
 		public void Place()
