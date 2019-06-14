9ccbe92fee0009a3cf4e82c1d110e3541cbacaac
Francelio Alencar <francelio.si@gmail.com>
2019-06-14 17:04:49 -0300

    Criando o Data Context

---
+++ BaltaStore.Shared/Settings.cs
@@ -0,0 +1,8 @@
+namespace BaltaStore.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Server=.\sqlexpress;Database=baltastore;User ID=saBalta;Password=1;";
+        
+    }    
+}
